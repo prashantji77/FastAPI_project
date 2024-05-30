337d49808c8bf5feb70eaacdcfcb3ed9efae7431
prashantji77 <prashantsharmaji1999@gmail.com>
2024-05-30 15:22:29 +0530

    commit

--- schemas/user.py
+++ schemas/user.py
@@ -1,15 +1,3 @@
-# def userEntity(item) -> dict:
-#     return{
-#         "id":str(item["_id"]),
-#         "name":str(item["name"]),
-#         "email":str(item["email"]),
-#         "password":str(item["password"]),
-
-#     }
-
-# def usersEntity(entity) -> list:
-#     return[userEntity(item) for item in entity]
-
 from typing import List
 from models.user import User
 
